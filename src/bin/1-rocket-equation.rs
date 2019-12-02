39d6920e6ec4d2a5ab0d7889e32f2d431ceb78fe
Phil Pluckthun <phil@kitten.sh>
2019-12-02 02:04:31 +0000

    Day 1

---
+++ src/bin/1-rocket-equation.rs
@@ -0,0 +1,23 @@
+// Example Usage:
+// $ 1-rocket-equation ./data/1-input
+
+use std::io::{BufReader,BufRead};
+use std::fs::File;
+use std::env;
+
+fn main () {
+  let path = match env::args().nth(1) {
+    None => panic!("File path must be passed"),
+    Some(str) => str,
+  };
+
+  let file = File::open(path).unwrap();
+  let reader = BufReader::new(file);
+  let fuel = reader
+    .lines()
+    .filter_map(|x| x.unwrap().parse::<i32>().ok())
+    .map(|x| x / 3 - 2)
+    .fold(0, |acc, x| acc + x);
+
+  println!("Total fuel consumption: {}", fuel);
+}
